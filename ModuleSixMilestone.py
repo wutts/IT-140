6b38cdbf7ec675987b4760bc7a9e0016e43a105f
w2flv <wutts@wutts.noreply.github.com>
2025-08-10 16:37:58 -0700

    initial check_direction() function
    Signed-off-by: w2flv <wutts@wutts.noreply.github.com>

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -9,6 +9,12 @@ rooms = {
     }
 # Setting current room to 'Great Hall'
 current_room = 'Great Hall'
+def check_direction(direction):
+    if direction == 'North' or direction == 'West' or direction == 'East' or direction == 'South' or direction == 'exit':
+        print(f'Moving in direction {direction}')
+    else:
+        print('Invalid direction. Please try using: West, East, North, South or exit')
+
 
 # Main loop
 while current_room != 'exit':
@@ -17,11 +23,12 @@ while current_room != 'exit':
     if user_direction == 'exit':
         current_room = 'exit'
     user_direction = user_direction.capitalize()
+    check_direction(user_direction)
     possible_directions = rooms[current_room]
 
     if user_direction in possible_directions:
         current_room = possible_directions[user_direction]
     else:
-        print('You cant go there')
+        pass
 
 
