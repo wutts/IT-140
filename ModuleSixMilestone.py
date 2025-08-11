b66ebde83196ac44262c34b5c687a130ad22b4f0
Alex Gorenkov <w2flv@Alexs-MBP.lan>
2025-08-10 20:23:06 -0700

    check_direction functionality

--- ModuleSixMilestone.py
+++ ModuleSixMilestone.py
@@ -7,14 +7,16 @@ rooms = {
         'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
         'Cellar': {'West': 'Bedroom'}
     }
+
 # Setting current room to 'Great Hall'
 current_room = 'Great Hall'
+# Checking if direction is correct and printing message in case it is not
 def check_direction(direction):
-    if direction == 'North' or direction == 'West' or direction == 'East' or direction == 'South' or direction == 'exit':
-        print(f'Moving in direction {direction}')
+    if direction in ('North', 'South', 'East', 'West'):
+        return True
     else:
-        print('Invalid direction. Please try using: West, East, North, South or exit')
-
+        print('Invalid direction, please try North, South, East, West or exit')
+        return False
 
 # Main loop
 while current_room != 'exit':
@@ -22,13 +24,13 @@ while current_room != 'exit':
     user_direction = input('Choose direction: ').strip().lower()
     if user_direction == 'exit':
         current_room = 'exit'
-    user_direction = user_direction.capitalize()
-    check_direction(user_direction)
-    possible_directions = rooms[current_room]
+        print('Thanks for playing!')
+        continue
 
+    possible_directions = rooms[current_room]
     if user_direction in possible_directions:
         current_room = possible_directions[user_direction]
     else:
-        pass
+        print('You can\'t go that way')
 
 
