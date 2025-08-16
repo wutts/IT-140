2aea0b0778dd9774657c95099ce2b91af6602d5b
wutts <>
2025-08-16 13:04:39 -0700

    Instructions function

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,1 +1,11 @@
 # Alexey Gorenkov
+inventory = []
+
+#Sample function showing the goal of the game and move commands
+def show_instructions():  
+   #print a main menu and the commands
+   print(f'Welcome to \'Against the System Text Game\', {player_name}')
+   print("Your goal is to collect all 6 core items before facing the final mirror.")
+   print("Move commands: go South, go North, go East, go West")
+   print("Add to Inventory: get 'item name'")
+
